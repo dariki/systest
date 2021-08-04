c2b026d6550d2cb26c2e9388fcc63126751f2ef8
Daniel Ramon Ariki <daniel.ariki@neon.com.br>
2021-08-04 14:22:37 -0300

    teste

---
+++ questao3/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+
+        Main.main(new String[]{"135"});
+
+    }
+
+
+}
